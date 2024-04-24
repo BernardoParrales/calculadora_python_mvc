adcfb5f117bc118740c512f0838ae010e6d7c375
victorequinox <victorequinox55@gmail.com>
2024-04-23 19:15:47 -0500

    Refactorizar CalculadoraVista

--- calculadora/vista.py
+++ calculadora/vista.py
@@ -8,6 +8,17 @@ class CalculadoraVista:
         self.ventana.geometry("320x500")
         self.ventana.resizable(0,0)
         
+        # Marcos o Frames
+        self.titulo()
+        self.operacion_en_tiempo_real()
+        self.entrada_de_valores()
+        self.titulo_historial()
+        self.mostrar_historial_en_tiempo_real()
+        
+        # Ejecutar la ventana
+        self.ventana.mainloop()
+        
+    def titulo(self):
         # Presenta el titulo de la APP | Frame
         self.marco_titulo_principal = Frame(self.ventana, height=30, width=320)
         self.marco_titulo_principal.config(
@@ -21,6 +32,7 @@ class CalculadoraVista:
         text_titulo = Label(self.marco_titulo_principal, text="CALCULADORA BÁSICA")
         text_titulo.pack(anchor=W)
         
+    def operacion_en_tiempo_real(self):
         # Mostrar la operación que se esta realizando | Frame
         self.marco_operacion = Frame(self.ventana, height=40, width=320)
         self.marco_operacion.config(
@@ -29,6 +41,7 @@ class CalculadoraVista:
         )
         self.marco_operacion.pack()
         
+    def entrada_de_valores(self):
         # Muestra los valores que se ingresan por teclado | Frame
         self.marco_ingreso_valores = Frame(self.ventana, height=60, width=320)
         self.marco_ingreso_valores.config(
@@ -37,6 +50,7 @@ class CalculadoraVista:
         )
         self.marco_ingreso_valores.pack()
         
+    def titulo_historial(self):
         # Titulo para el historial de operaciones | Frame
         self.marco_titulo_historial = Frame(self.ventana, height=30, width=320)
         self.marco_titulo_historial.config(
@@ -45,6 +59,7 @@ class CalculadoraVista:
         )
         self.marco_titulo_historial.pack()
         
+    def mostrar_historial_en_tiempo_real(self):
         # Muestra el historial de las operaciones realizadas | Frame
         self.historial = Frame(self.ventana, height=340, width=320)
         self.historial.config(
@@ -53,7 +68,9 @@ class CalculadoraVista:
         )
         self.historial.pack()
         
-        self.ventana.mainloop()
+        
+
+
 
 cal = CalculadoraVista()
 
